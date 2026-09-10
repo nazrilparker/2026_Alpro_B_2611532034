2fee827dc69b11eff814d5caf78b7d488219a276
M. Nazril <fmuhammadnazrilfandhi@gmail.com>
2026-09-10 09:47:02 +0700

    Add files via upload

---
+++ Pekan2/String_2611532034.py
@@ -0,0 +1,15 @@
+# Buat file dengan nama String_NIM.py
+#nama variabel ditambah 4 digit nim terakhir contoh: nama_1234
+#ganti program nya dan data masing-masing
+nama ="Muhammad Nazril Fandhi_2611532034"
+jenis_kelamin = 'L'
+alamat = """
+    Asrama Unand
+    Kec.pauh
+    Kota Padang
+"""
+agama = 'islam'
+print ("Nama saya adalah: ", nama)
+print ("Jenis kelamin saya adalah: ", jenis_kelamin)
+print ("Alamat saya adalah: ", alamat)
+print ("Agama saya adalah: ", agama)
---
+++ Pekan2/numerik_2611532034.py
@@ -0,0 +1,13 @@
+# Buat file dengan nama Numerik_2611532034.py
+# nama variabel ditambah 4 digit terakhir NIM contoh: a_5500
+#create a variable with integer value.
+a_2034=100
+print("The type of variable having value", a_2034, " is ", type (a_2034))
+
+#create a variable with float value.
+b_2034=10.2345
+print("The type of variable having value", b_2034, " is ", type (b_2034))
+
+#create a variable with complex value.
+c_2034=100+3j
+print("The type of variable having value", c_2034, " is ", type (c_2034))
